54c01c77b323884a6d9a68e8596cf7168b8809a0
Harald Sømme <haraldsoemme@gmail.com>
2024-09-16 14:00:57 +0200

    fix tailwind and cleanup

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
