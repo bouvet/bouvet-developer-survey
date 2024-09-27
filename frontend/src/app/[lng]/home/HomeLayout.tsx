417a75a4b144c2ebeb3a0b87d1e1fcd8886fe377
haraldsomme <haraldsoemme@gmail.com>
2024-09-27 10:41:12 +0200

    Added plotly and created example chart component

--- frontend/src/app/[lng]/home/HomeLayout.tsx
+++ frontend/src/app/[lng]/home/HomeLayout.tsx
@@ -2,6 +2,7 @@
 
 import { Header } from "@/app/components/Header";
 import { HomeContextProvider } from "./context";
+import DotPlotChart from "@/app/components/charts/DotPlotChart";
 
 // Component
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <HomeContextProvider>
       <Header />
       <h1 className="text-2xl underline">Bouvet Developer Survey</h1>
+      <DotPlotChart />
     </HomeContextProvider>
   );
 }
--- frontend/src/app/components/charts/DotPlotChart.tsx
+++ frontend/src/app/components/charts/DotPlotChart.tsx
@@ -1,9 +1,88 @@
-import React from "react";
+"use client";
+import Plot from 'react-plotly.js';
 
+/*
 type chartProps = {
     data: { name: string; value: number };
 };
+*/
 
-export default function DotPlotChart(props: chartProps) {
-    return props.data;
+export default function DotPlotChart() {
+
+    const tech = ['C#','Typescript','Python','Javascript','SQL','HTML/CSS','Rust','Bash/Shell','PoweShell','Go'].reverse();
+    const admired = [46,40].reverse();
+    const desired = [68,65,68,44,53,53,86,52,39,35].reverse();
+    
+    const trace1 = {
+      type: 'scatter',
+      x: admired,
+      y: tech,
+      mode: 'markers+text',
+      name: 'Ønsket',
+      text: admired.map(value => value + ' %'),
+      textposition: 'left',
+      marker: {
+        color: 'rgba(156, 165, 196, 0.95)',
+        line: {
+          color: 'rgba(156, 165, 196, 1.0)',
+          width: 1,
+        },
+        symbol: 'circle',
+        size: 12
+      }
+    };
+    
+    const trace2 = {
+      x: desired,
+      y: tech,
+      mode: 'markers+text',
+      name: 'Beundret',
+      text: desired.map(value => value + ' %'),
+      textposition: 'right',
+      marker: {
+        color: 'rgba(204, 204, 204, 0.95)',
+        line: {
+          color: 'rgba(217, 217, 217, 1.0)',
+          width: 1,
+        },
+        symbol: 'circle',
+        size: 12
+      }
+    };
+    
+    const layout = {
+      title: 'Beundret og ønsket',
+      xaxis: {
+        visible:false,
+        range: [0, 100]
+      },
+      margin: {
+        l: 80,
+        r: 40,
+        b: 50,
+        t: 80
+      },
+      legend: {
+        font: {
+          size: 10,
+          xanchor:'right'
+        },
+    
+        orientation: 'h'
+      },
+      autosize: true,
+      paper_bgcolor: 'rgb(254, 247, 234)',
+      plot_bgcolor: 'rgb(254, 247, 234)',
+    };
+    
+    const config = {responsive: true}
+    const data = [trace1, trace2];
+
+    return (
+        <Plot
+            data={data}
+            layout={layout}
+            config={config}
+        />
+    );
 }
