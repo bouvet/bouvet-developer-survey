87f59762860de9372f79b78b02405105a82de134
haraldsomme <haraldsoemme@gmail.com>
2024-10-10 14:27:41 +0200

    chart config file, styling

--- frontend/src/app/components/charts/DotPlotChartJson.tsx
+++ frontend/src/app/components/charts/DotPlotChartJson.tsx
@@ -1,91 +1,71 @@
 "use client";
-import dynamic from 'next/dynamic';
-import chartData from './chartData.json';
+import dynamic from "next/dynamic";
+import chartData from "./chartData.json";
+import { chartConfig } from "./chartConfig";
 
 // lazy load 'react-plotly.js'
-const Plot = dynamic(() => import('react-plotly.js'), { 
+const Plot = dynamic(() => import("react-plotly.js"), {
   ssr: false,
-  loading: () => <p>Laster...</p>
+  loading: () => <p>Laster...</p>,
 });
 
-// Graph colors
-const dotMinColor =     '#F9A86F';
-const dotMaxColor =     '#1D43C6';
-const dottedLineColor = '#ddd';
-const thickLineColor =  '#ccc';
-const dottedLineWidth =  1;
-const thickLineWidth =   3;
-const markerSize =       15;
-const graphBackgroundColor = '#faf1e3';
-const fontColor =        '#555';
-const fontSize =         11;
-
 const DotPlotChartJson = () => {
-
-    const traces = chartData.data.map((trace) => {
-      return {
-        x: [trace.xMin, trace.xMax],
-        y: new Array(2).fill(trace.label), // create an array with the same y label for each value
-        mode: 'lines+markers+text',
-        name: trace.label,
-        type: 'scatter',
-        text: [trace.xMin + " %", trace.xMax + " %"],
-        textposition: ["left", "right"],
-        marker: {
-          color: [dotMinColor, dotMaxColor],
-          symbol: 'circle',
-          size: markerSize,
-
-        },
-        line: { 
-          color: thickLineColor,
-          width: thickLineWidth
-        }
-      };
-    });
-
-    const layout = {
-      title: chartData.name,
-      xaxis: {
-          showgrid: false,
-          visible: false,
+  const traces = chartData.data.map((trace) => {
+    return {
+      x: [trace.xMin, trace.xMax],
+      y: new Array(2).fill(trace.label), // create an array with the same y label for each value
+      mode: "lines+markers+text",
+      name: trace.label,
+      type: "scatter",
+      text: [trace.xMin + " %", trace.xMax + " %"],
+      textposition: ["left", "right"],
+      marker: {
+        color: [chartConfig.dotMinColor, chartConfig.dotMaxColor],
+        symbol: "circle",
+        size: chartConfig.markerSize,
       },
-      yaxis: {
-        showline: false,
-        gridwidth: dottedLineWidth,
-        gridcolor: dottedLineColor,
-
+      line: {
+        color: chartConfig.thickLineColor,
+        width: chartConfig.thickLineWidth,
       },
-      margin: {
-        l: 80,
-        r: 40,
-        b: 50,
-        t: 80
-      },
-      legend: {
-        visible: false
-      },
-
-      font: {
-        color: fontColor,
-        size: fontSize
-
-      },
-      autosize: true,
-      paper_bgcolor: graphBackgroundColor,
-      plot_bgcolor: graphBackgroundColor,
     };
+  });
 
-    const config = {responsive: true, displayModeBar: false};
+  const layout = {
+    title: chartData.name,
+    xaxis: {
+      showgrid: false,
+      visible: false,
+    },
+    yaxis: {
+      showline: false,
+      gridwidth: chartConfig.dottedLineWidth,
+      gridcolor: chartConfig.dottedLineColor,
+    },
+    margin: {
+      l: 80,
+      r: 40,
+      b: 50,
+      t: 80,
+    },
+    legend: {
+      visible: false,
+    },
 
-    return (
-        <Plot
-            className="w-full"
-            data={traces}
-            layout={layout}
-            config={config}
-        />
-    );
-}
+    font: {
+      color: chartConfig.fontColor,
+      size: chartConfig.fontSize,
+    },
+    autosize: true,
+    paper_bgcolor: chartConfig.graphBackgroundColor,
+    plot_bgcolor: chartConfig.graphBackgroundColor,
+  };
+
+  const config = { responsive: true, displayModeBar: false };
+
+  return (
+    <Plot className="w-full" data={traces} layout={layout} config={config} />
+  );
+};
 
 export default DotPlotChartJson;
