a91aa5ff444c395c419388a6837c0aefb7f6e2b0
Ørjan <dybevik2@gmail.com>
2024-10-21 09:24:01 +0200

    Added barchart with mock data

---
+++ frontend/src/app/components/charts/barchart/barchartConfig.ts
@@ -0,0 +1,6 @@
+export const barchartConfig = {
+  graphBackgroundColor: "#faf1e3",
+  fontColor: "#555",
+  fontSize: 11,
+  barColor: "#11133c",
+};
--- frontend/src/app/components/survey/Survey.tsx
+++ frontend/src/app/components/survey/Survey.tsx
@@ -1,31 +1,57 @@
-import React from 'react';
-import DotPlotChart from '@/app/components/charts/DotPlotChart';
-import DotPlotChartJson from '@/app/components/charts/DotPlotChartJson';
+import React from "react";
+import DotPlotChart from "@/app/components/charts/DotPlotChart";
+import DotPlotChartJson from "@/app/components/charts/DotPlotChartJson";
 import { useClientTranslation } from "../../../../shared/i18n/src";
+import BarChart from "../charts/barchart/BarChart";
+import BarChartJson from "../charts/barchart/BarChartJson";
 
 const Survey = () => {
-    const { t } = useClientTranslation();
+  const { t } = useClientTranslation();
 
-    // Chart data example
-    const chartData = {
-        title: t("admired-and-desired"),
-        x1: [20, 30, 10],
-        x2: [60, 50, 80],
-        y: ["C#", "Typescript", "Python"],
-    };
+  // Chart data example
+  const chartData = {
+    title: t("admired-and-desired"),
+    x1: [20, 30, 10],
+    x2: [60, 50, 80],
+    y: ["C#", "Typescript", "Python"],
+  };
 
-    
-    return (
-        <section className="mx-auto flex flex-col max-w-7xl lg:px-8">
-            <div className="w-full">
-                {/*
+  const barchartData = {
+    title: "Programmeringsspråk: Topp 10",
+    x: [10, 12, 36, 40, 43, 51, 52, 57, 58, 65],
+    y: [
+      "Go",
+      "Java",
+      "Bash/Shell (all shells)",
+      "PowerShell",
+      "Python",
+      "HTML/CSS",
+      "TypeScript",
+      "C#",
+      "SQL",
+      "JavaScript",
+    ],
+  };
+
+  return (
+    <div>
+      <section className="mx-auto flex flex-col max-w-7xl lg:px-8">
+        <div className="w-full">
+          {/*
                 <DotPlotChart {...chartData} />
                 */}
-                <DotPlotChartJson/>
-            </div>
-        </section>
-    );
+          <DotPlotChartJson />
+        </div>
+      </section>
+      <section className="mx-auto flex flex-col max-w-7xl lg:px-8">
+        <div className="w-full">
+          <BarChart {...barchartData} />
 
+          {/* <BarChartJson /> */}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Survey;
