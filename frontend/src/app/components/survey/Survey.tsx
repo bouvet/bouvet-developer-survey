91df637a9c07f187b45ac6c28ae5a4aaf7854e48
haraldsomme <haraldsoemme@gmail.com>
2024-11-07 09:58:10 +0100

    rename

---
+++ frontend/src/app/components/survey/Survey.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { useSurveyStructure } from "@/app/hooks/useSurveyStructure";
+import SurveyAnswers from "./SurveyAnswers";
+import { Question, SurveyBlock, BlockElement } from "@/app/types/survey";
+
+const Survey = () => {  
+  // Get survey structure data
+  const { data, error, isLoading } = useSurveyStructure();
+
+  return (
+    <div className="mx-auto flex flex-col max-w-7xl lg:px-8">
+      {isLoading && <div>Henter undersøkelsen...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {!isLoading && !error && (
+        <>
+          {data.surveyBlocks.map((block: SurveyBlock, index: number) => {
+            return (
+              <section key={index}>
+                {block.blockElements.map((element: BlockElement) => {
+                  return element.questions.map((question: Question) => (
+                    <div key={question.id}>
+                      <SurveyAnswers questionId={question.id} />
+                    </div>
+                  ));
+                })}
+              </section>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Survey;
