7f91e0ddf22d07a093bcb0d89dbe2ac8eb9a7c05
haraldsomme <haraldsoemme@gmail.com>
2024-11-13 13:44:18 +0100

    gracefull handle missing or wrong survey data

--- frontend/src/app/lib/fetcher.ts
+++ frontend/src/app/lib/fetcher.ts
@@ -1,7 +1,12 @@
 export const fetcher = async (url: string) => {
-    const res = await fetch(url)
+  try {
+    const res = await fetch(url);
     if (!res.ok) {
-      throw new Error('API Error: ' + res.statusText)
+      throw new Error('API Error: ' + res.statusText);
     }
-    return res.json()
+    return await res.json();
+  } catch (error) {
+    // Handle the error gracefully without logging to the console
+    return { error: 'Failed to fetch data' };
   }
+};
