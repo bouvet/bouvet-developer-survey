f296285d946588a1e7d38ca404a514a39fb6cb8f
haraldsomme <haraldsoemme@gmail.com>
2024-11-14 10:41:01 +0100

    remove unused context

--- frontend/src/app/[lng]/client.tsx
+++ frontend/src/app/[lng]/client.tsx
@@ -1,20 +1,10 @@
 "use client";
 
-import { RtkQueryFetcherStatus } from "../../../shared/rtk/src";
 import HomeLayout from "./layout";
 
 // Component
 export default function HomeClient() {
-  // !todo const homeQuery = useHomeQuery(ENUM);
-  // !todo const currentUserQuery = useCurrentUserQuery();
-
   return (
-    <RtkQueryFetcherStatus
-      errorNotificationQueries={[]}
-      queries={[]}
-      queriesProgressText={["homeQuery"]}
-    >
       <HomeLayout />
-    </RtkQueryFetcherStatus>
   );
 }
--- frontend/src/app/[lng]/layout.tsx
+++ frontend/src/app/[lng]/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Header } from "@/app/components/Header";
-import { HomeContextProvider } from "./context/HomeContextProvider";
 import Survey from "@/app/components/survey/SurveyWithMockData";
 import Hero from "@/app/components/hero/Hero";
 import Top10 from "@/app/components/survey/Top10";
@@ -11,15 +10,14 @@ import Summary from "@/app/components/summary/Summary";
 export default function Home() {
   // Render
   return (
-    <HomeContextProvider>
-      <Header />
-      {/* Remove tailwind scrolling on main element to enable testing */}
-      <main className="scroll-smooth">
-        <Hero />
-        <Summary />
-        <Top10 />
-        <Survey />
+      <>
+        <Header />
+        <main className="scroll-smooth">
+          <Hero />
+          <Summary />
+          <Top10 />
+          <Survey />
       </main>
-    </HomeContextProvider>
+    </>
   );
 }
