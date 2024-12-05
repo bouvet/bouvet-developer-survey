beb7d555a5107ae7d7f409fd50c10fee07b35ae0
crlsocro <christian.r.lie@hotmail.no>
2024-12-05 11:27:21 +0100

    feat: remove AnswerOption entity and related DTOs; add GetResponseDto for survey results

---
+++ Bouvet.Developer.Survey.Backend/Bouvet.Developer.Survey.Service/TransferObjects/Survey/Results/Response/GetStatsDto.cs
@@ -0,0 +1,7 @@
+namespace Bouvet.Developer.Survey.Service.TransferObjects.Survey.Results.Response;
+
+public class GetResponseDto
+{
+    public int Admired { get; set; }
+    public int Desired { get; set; }
+}
