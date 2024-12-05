cdf18ac8213f320e8f6cfd0360db19c214e36166
haraldsomme <haraldsoemme@gmail.com>
2024-12-05 09:52:27 +0100

    rename survey component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Survey from "@/app/components/survey/SurveyWithMockData";
+import Survey from "@/app/components/survey/Survey";
 import Hero from "@/app/components/hero/Hero";
 import Summary from "@/app/components/summary/Summary";
 
