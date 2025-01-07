776825ffd57dbcd5e0b816d65c436794397e3443
idatorgersen <ida.torgersen@bouvet.no>
2025-01-07 15:30:42 +0100

    Dynamic logo year, added notfound-page

--- frontend/src/app/components/Header/components/HeaderTitle.tsx
+++ frontend/src/app/components/Header/components/HeaderTitle.tsx
@@ -1,11 +1,14 @@
 import Logo from "@/app/components/Header/Logo";
+import { useParams } from "next/navigation";
 
 export default function HeaderTitle() {
-  // Render
+  // TODO: Error handling for non-existing params when we get real data from backend
+  const { year } = useParams<{ year?: string }>();
+
   return (
     <div className="flex lg:flex-1 pr-5">
       <a href="/" className="-m-1.5 p-1.5">
-        <Logo />
+        <Logo year={year} />
       </a>
     </div>
   );
