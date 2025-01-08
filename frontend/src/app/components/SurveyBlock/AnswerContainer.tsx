c1479e7f0a8b1840f378f5ebe039b7f2b3c97d06
Jone-Torgersen <jone.torgersen@bouvet.no>
2025-01-08 16:07:13 +0100

    fix: unify chart counter position for all charts
    - Added flex-1 container around children in answerContiner
    - Removed absolute positioning on chart counter in favor of margin top auto to ensure
    - Added flex and updated chart-container style to allow a uniform positioning for the chartcounter

--- frontend/src/app/components/SurveyBlock/AnswerContainer.tsx
+++ frontend/src/app/components/SurveyBlock/AnswerContainer.tsx
@@ -9,6 +9,7 @@ const AnswerContainer = ({ tabs, children }: Props) => {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
+<<<<<<< HEAD
     <div className={`bg-[#11133c]  flex-[5] relative`}>
       {!!tabs?.length && (
         <div className="flex bg-[#11133c] p-4 gap-8 justify-center">
@@ -29,6 +30,32 @@ const AnswerContainer = ({ tabs, children }: Props) => {
         </div>
       )}
       {Array.isArray(children) ? children[activeTab] : children}
+=======
+    <div className={`bg-[#3b3f45] rounded-2xl flex-[5] shadow-md`}>
+      <div className="relative flex flex-col h-full">
+        {!!tabs?.length && (
+          <div className="flex bg-[#2f3237] p-4 rounded-2xl rounded-b-none gap-8 justify-center">
+            {tabs.map((tab, index) => (
+              <button
+                key={index}
+                className={`flex-1 text-center font-bold text-sm rounded-md max-w-40 h-8
+                  ${
+                    activeTab === index
+                      ? "text-black bg-slate-50"
+                      : "text-white hover:text-blue-400"
+                  }`}
+                onClick={() => setActiveTab(index)}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+        )}
+        <div className="flex-1">
+          {Array.isArray(children) ? children[activeTab] : children}
+        </div>
+      </div>
+>>>>>>> 98bc988 (fix: unify chart counter position for all charts)
     </div>
   );
 };
