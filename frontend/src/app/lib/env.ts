0025f83f36f61aca270020486e01a7ba80e2d9f6
asleboon <asle.berge@gmail.com>
2025-01-09 10:29:37 +0100

    fix: environment config

--- frontend/src/app/lib/env.ts
+++ frontend/src/app/lib/env.ts
@@ -4,14 +4,11 @@ const envSchema = z.object({
   apiUrl: z
     .string({
       required_error:
-        "NEXT_PUBLIC_API_URL is required in your env.local file",
+        "NEXT_PUBLIC_API_URL is required your .env.local file",
     })
     .url("NEXT_PUBLIC_API_URL must be an URL"),
 });
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
 export const environment = envSchema.parse({
-  surveyStructureEndpoint: `${apiUrl}/v1/results`,
-  surveyAnswersEndpoint: `${apiUrl}/v1/results/getQuestionById`
+  apiUrl: process.env.NEXT_PUBLIC_API_URL,
 });
