390a4ea6b1bd9a6a7de76003f3fe8609a1bfe05d
idatorgersen <ida.torgersen@bouvet.no>
2025-01-09 12:58:55 +0100

    wip, failed merge

--- frontend/src/app/components/SurveyBlock/AnswerContainer.tsx
+++ frontend/src/app/components/SurveyBlock/AnswerContainer.tsx
@@ -9,41 +9,19 @@ const AnswerContainer = ({ tabs, children }: Props) => {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-<<<<<<< HEAD
     <div className={`bg-[#11133c]  flex-[5] relative`}>
-      {!!tabs?.length && (
-        <div className="flex bg-[#11133c] p-4 gap-8 justify-center">
-          {tabs.map((tab, index) => (
-            <button
-              key={index}
-              className={`flex-1 text-center font-bold text-sm  max-w-40 h-8
+      <div className="relative flex flex-col h-full">
+        {!!tabs?.length && (
+          <div className="flex bg-[#11133c] p-4 gap-8 justify-center">
+            {tabs.map((tab, index) => (
+              <button
+                key={index}
+                className={`flex-1 text-center font-bold text-sm  max-w-40 h-8
                         ${
                           activeTab === index
                             ? "text-black bg-slate-50"
                             : "text-white hover:text-blue-400 "
                         }`}
-              onClick={() => setActiveTab(index)}
-            >
-              {tab}
-            </button>
-          ))}
-        </div>
-      )}
-      {Array.isArray(children) ? children[activeTab] : children}
-=======
-    <div className={`bg-[#3b3f45] rounded-2xl flex-[5] shadow-md`}>
-      <div className="relative flex flex-col h-full">
-        {!!tabs?.length && (
-          <div className="flex bg-[#2f3237] p-4 rounded-2xl rounded-b-none gap-8 justify-center">
-            {tabs.map((tab, index) => (
-              <button
-                key={index}
-                className={`flex-1 text-center font-bold text-sm rounded-md max-w-40 h-8
-                  ${
-                    activeTab === index
-                      ? "text-black bg-slate-50"
-                      : "text-white hover:text-blue-400"
-                  }`}
                 onClick={() => setActiveTab(index)}
               >
                 {tab}
@@ -51,11 +29,10 @@ const AnswerContainer = ({ tabs, children }: Props) => {
             ))}
           </div>
         )}
-        <div className="flex-1">
-          {Array.isArray(children) ? children[activeTab] : children}
-        </div>
       </div>
->>>>>>> 98bc988 (fix: unify chart counter position for all charts)
+      <div className="flex-1">
+        {Array.isArray(children) ? children[activeTab] : children}
+      </div>
     </div>
   );
 };
