053ea76133f6b9115bd180c2994071d7b8eb215f
asleboon <asle.berge@gmail.com>
2025-01-09 10:46:38 +0100

    fix: wrong urls

--- frontend/src/app/hooks/useSurveyStructure.ts
+++ frontend/src/app/hooks/useSurveyStructure.ts
@@ -1,16 +1,20 @@
 import { fetcher } from "../lib/fetcher";
 import useSWR from "swr";
-import { Survey } from '../types/survey';
+import { Survey } from "../types/survey";
 import { environment } from "../lib/env";
 
-export const useSurveyStructure = (): { data: Survey, error: {message: string}, isLoading: boolean} => {
-    const { data, error, isLoading } = useSWR(
-        `${environment.apiUrl}/v1/results/getQuestionById/d01e692d-0374-4629-a3a9-52a16aa0acf1`,
-        fetcher
-    )
-    return { 
-        data,
-        error,
-        isLoading
-    }
+export const useSurveyStructure = (): {
+  data: Survey;
+  error: { message: string };
+  isLoading: boolean;
+} => {
+  const { data, error, isLoading } = useSWR(
+    `${environment.apiUrl}/v1/results/d01e692d-0374-4629-a3a9-52a16aa0acf1`,
+    fetcher
+  );
+  return {
+    data,
+    error,
+    isLoading,
+  };
 };
