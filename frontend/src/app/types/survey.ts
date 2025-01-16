23dd4413047cac5ebd64a79e45a5fda0be9ed05a
asleboon <asle.berge@gmail.com>
2025-01-16 15:36:23 +0100

    fix: dataExportTag

--- frontend/src/app/types/survey.ts
+++ frontend/src/app/types/survey.ts
@@ -1,56 +1,56 @@
 // Base interface for common fields
 export interface BaseEntity {
-    id: string;
-    createdAt: string;
-    updatedAt: string | null;
-  }
+  id: string;
+  createdAt: string;
+  updatedAt: string | null;
+}
 
-  export interface Question extends BaseEntity {
-    blockElementId: string;
-    dateExportTag: string; // TODO: fix typo when fixed in backend 
-    questionText: string;
-    questionDescription: string;
-    isMultipleChoice: boolean;
-  }
+export interface Question extends BaseEntity {
+  blockElementId: string;
+  dataExportTag: string;
+  questionText: string;
+  questionDescription: string;
+  isMultipleChoice: boolean;
+}
 
-  export interface BlockElement extends BaseEntity {
-    type: string;
-    questions: Question[];
-  }
+export interface BlockElement extends BaseEntity {
+  type: string;
+  questions: Question[];
+}
 
-  export interface SurveyBlock extends BaseEntity {
-    surveyId: string;
-    type: string;
-    description: string;
-    blockElements: BlockElement[];
-  }
-  
-  export interface Survey extends BaseEntity {
-    name: string;
-    surveyId: string;
-    lastSyncedAt: string;
-    surveyBlocks: SurveyBlock[];
-  }
+export interface SurveyBlock extends BaseEntity {
+  surveyId: string;
+  type: string;
+  description: string;
+  blockElements: BlockElement[];
+}
 
-  export interface Answer extends BaseEntity {
-    id: string;
-    dateExportTag: string; // TODO: fix typo when fixed in backend 
-    questionText: string;
-    questionDescription: string;
-    isMultipleChoice: boolean;
-    createdAt: string;
-    updatedAt: string | null;
-    choices: Choice[];
-  }
+export interface Survey extends BaseEntity {
+  name: string;
+  surveyId: string;
+  lastSyncedAt: string;
+  surveyBlocks: SurveyBlock[];
+}
+
+export interface Answer extends BaseEntity {
+  id: string;
+  dataExportTag: string;
+  questionText: string;
+  questionDescription: string;
+  isMultipleChoice: boolean;
+  createdAt: string;
+  updatedAt: string | null;
+  choices: Choice[];
+}
 
 export interface Choice extends BaseEntity {
-    id: string;
-    text: string;
-    indexId: number;
-    createdAt: string;
-    updatedAt: string | null;
-    responses: Response[];
-  }
+  id: string;
+  text: string;
+  indexId: number;
+  createdAt: string;
+  updatedAt: string | null;
+  responses: Response[];
+}
 
 export interface Response extends BaseEntity {
   id: string;
