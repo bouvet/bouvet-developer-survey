00b9d467177c06a2dbab89cf87294e6375eb5e82
Jone-Torgersen <jone.torgersen@bouvet.no>
2025-01-22 13:22:04 +0100

    chore: add bearer token to api calls
    - refactored fetcher to get token using msal and add auth header to all requests
    - enabled authentication on endpoints

--- frontend/src/app/lib/fetcher.ts
+++ frontend/src/app/lib/fetcher.ts
@@ -1,12 +1,33 @@
+import { scopes } from '../auth/authConfig';
+import { msalInstance } from '../auth/authProvider';
+
 export const fetcher = async (url: string) => {
   try {
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error('API Error: ' + res.statusText);
+    const accounts = msalInstance.getAllAccounts();
+    if (accounts.length === 0) {
+      throw new Error("No active account found. Please sign in.");
     }
+
+    const accessTokenResponse = await msalInstance.acquireTokenSilent({
+      account: accounts[0],
+      scopes,
+    });
+
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${accessTokenResponse.accessToken}`,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`API Error: ${res.statusText}`);
+    }
+
     return await res.json();
-  } catch (error) {
-    // Handle the error gracefully without logging to the console
-    return { error: 'Failed to fetch data' };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+    return { error: "Failed to fetch data" };
   }
 };
