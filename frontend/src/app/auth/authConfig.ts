8ef91b5ed77a4b87f9c47123b0d19e7045e0e9d4
Jone-Torgersen <jone.torgersen@bouvet.no>
2025-01-23 10:35:45 +0100

    fix: remove redirect uri

--- frontend/src/app/auth/authConfig.ts
+++ frontend/src/app/auth/authConfig.ts
@@ -5,7 +5,6 @@ export const msalConfig = {
   auth: {
     clientId: '2f3f1b63-3f3d-4389-bf65-c43d09ff8aa5', 
     authority: `https://login.microsoftonline.com/c317fa72-b393-44ea-a87c-ea272e8d963d`, 
-    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI, 
   },
   cache: {
     cacheLocation: "localStorage", 
