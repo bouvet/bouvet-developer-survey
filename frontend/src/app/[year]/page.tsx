617e418e2ae3dd2ef9cb64b2cf1a7ac22aeca9ee
Jone-Torgersen <jone.torgersen@bouvet.no>
2025-01-28 09:01:46 +0100

    chore: fix merge conflicts

--- frontend/src/app/[year]/page.tsx
+++ frontend/src/app/[year]/page.tsx
@@ -3,34 +3,13 @@
 import Survey from "@/app/components/survey/Survey";
 import Hero from "@/app/components/hero/Hero";
 import Summary from "@/app/components/summary/Summary";
-<<<<<<< HEAD
-import { useParams } from "next/navigation";
-import Link from "next/link";
-import { MsalAuthenticationTemplate } from "@azure/msal-react";
-import { InteractionType } from "@azure/msal-browser";
-
-export default function SurveyPage() {
-  const { year } = useParams<{ year: string }>();
-  if (year !== "2024")
-    return (
-      <main>
-        <section id="intro" className="survey-section">
-          <h3 className="mt-10 font-bold text-left">
-            Ingen data for denne perioden
-          </h3>
-          <Link href="/" className="underline">
-            Gå tilbake
-          </Link>
-        </section>
-      </main>
-    );
-=======
 import { notFound, useParams } from "next/navigation";
+import { MsalAuthenticationTemplate } from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
 
 export default function SurveyPage() {
   const { year } = useParams<{ year: string }>();
   if (year !== "2024") notFound();
->>>>>>> feat/92-change-logo-to-support-dynamic-year
   return (
     <main>
       <MsalAuthenticationTemplate interactionType={InteractionType.Redirect}>
