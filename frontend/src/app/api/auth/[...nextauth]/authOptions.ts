bf42c64b772a76ba5601673d850c840ce629ce79
Albin Karlsson <albin.karlsson@bouvet.no>
2025-04-23 09:24:32 +0200

    feat: 169 refetch token

--- frontend/src/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -4,7 +4,16 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
-    idToken?: string;
+    error?: "RefreshTokenError";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken: string;
+    expiresAt: number;
+    refreshToken?: string;
+    error?: "RefreshTokenError";
   }
 }
 
@@ -16,7 +25,7 @@ const authOptions: AuthOptions = {
       tenantId: process.env.AZURE_AD_TENANT_ID,
       authorization: {
         params: {
-          scope: `openid profile email api://${process.env.AZURE_AD_BACKEND_CLIENT_ID}/user_impersonation`,
+          scope: `openid profile email api://${process.env.AZURE_AD_BACKEND_CLIENT_ID}/user_impersonation offline_access`,
           prompt: "login",
         },
       },
@@ -25,14 +34,60 @@ const authOptions: AuthOptions = {
   callbacks: {
     async session({ session, token }) {
       session.accessToken = token.accessToken as string;
+      if (token.error) console.log("Error token in Session", token.error);
 
       return session;
     },
     async jwt({ token, account }) {
       if (account) {
-        token.accessToken = account.access_token;
+        token.accessToken = account.access_token as string;
+        token.expiresAt = account.expires_at as number;
+        token.refreshToken = account.refresh_token;
+        return token;
+      } else if (Date.now() < token.expiresAt! * 1000) {
+        return token;
+      } else {
+        if (!token.refreshToken) throw new Error("Invalid refresh token");
+
+        try {
+          const response: Response = await fetch(
+            `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/authorize`,
+            {
+              method: "POST",
+              body: new URLSearchParams({
+                client_id: process.env.AZURE_AD_CLIENT_ID as string,
+                client_secret: process.env.AZURE_AD_CLIENT_SECRET as string,
+                grant_type: "refresh_token",
+                refresh_token: token.refreshToken!,
+              }),
+            }
+          );
+          const tokensOrError = await response.json();
+
+          if (!response.ok) throw tokensOrError;
+
+          const newTokens = tokensOrError as {
+            access_token: string;
+            expires_in: number;
+            refresh_token?: string;
+          };
+
+          console.log("Creating new token: ", newTokens);
+
+          return {
+            ...token,
+            accessToken: newTokens.access_token,
+            expiresAt: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+            refreshToken: newTokens.refresh_token
+              ? newTokens.refresh_token
+              : token.refreshToken,
+          };
+        } catch (error) {
+          console.error("Error refreshing accessToken", error);
+          token.error = "RefreshTokenError";
+          return token;
+        }
       }
-      return token;
     },
   },
 };
