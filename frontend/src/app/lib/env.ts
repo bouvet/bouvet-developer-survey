5c3d36909a502acb14f465c68755178687e280cf
Albin Karlsson <albin.karlsson@bouvet.no>
2025-04-30 10:12:25 +0200

    feat: 169 docker config troubleshooting #7

--- frontend/src/app/lib/env.ts
+++ frontend/src/app/lib/env.ts
@@ -3,17 +3,19 @@ import { z } from "zod";
 const envSchema = z.object({
   apiUrl: z
     .string({
-      required_error: "NEXT_PUBLIC_API_URL is required your .env.local file",
+      required_error:
+        "NEXT_PUBLIC_API_URL is required in your .env.local/production file",
     })
     .url("NEXT_PUBLIC_API_URL must be an URL"),
-  AZURE_AD_CLIENT_SECRET: z
+  /*AZURE_AD_CLIENT_SECRET: z
     .string({
-      required_error: "AZURE_AD_CLIENT_SECRET is required your .env.local file",
+      required_error:
+        "AZURE_AD_CLIENT_SECRET is required in your .env.local/production file",
     })
-    .url("AZURE_AD_CLIENT_SECRET must be an value"),
+    .startsWith("MgE8Q"),*/
 });
 
 export const environment = envSchema.parse({
   apiUrl: process.env.NEXT_PUBLIC_API_URL,
-  AZURE_AD_CLIENT_SECRET: process.env.AZURE_AD_CLIENT_SECRET as string,
+  AZURE_AD_CLIENT_SECRET: process.env.AZURE_AD_CLIENT_SECRET,
 });
