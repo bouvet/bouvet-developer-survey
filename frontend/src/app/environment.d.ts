1b0415d96484fba7c73d85c5aff3b8f97a59bee4
Albin Karlsson <albin.karlsson@bouvet.no>
2025-04-30 10:12:25 +0200

    feat: 169 docker config troubleshooting #7

--- frontend/src/app/environment.d.ts
+++ frontend/src/app/environment.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-      NEXT_PUBLIC_REDIRECT_URI: string;
-      NEXT_PUBLIC_API_URL:string;
-    }
+  interface ProcessEnv {
+    NEXT_PUBLIC_REDIRECT_URI: string;
+    NEXT_PUBLIC_API_URL: string;
+    AZURE_AD_CLIENT_SECRET: string;
   }
+}
