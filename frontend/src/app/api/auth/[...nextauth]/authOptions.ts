ef557d36ad93ca30d231489b7fb974804c70a05d
Albin Karlsson <albin.karlsson@bouvet.no>
2025-05-09 13:34:01 +0200

    feat: 169 clean up

--- frontend/src/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -23,85 +23,80 @@ const tenantId = process.env.AZURE_AD_TENANT_ID as string;
 const backendClientId = process.env.AZURE_AD_BACKEND_CLIENT_ID as string;
 const nextAuthSecret = process.env.NEXTAUTH_SECRET as string;
 
-const authOptions : AuthOptions = {
-      secret: nextAuthSecret,
-      providers: [
-        AzureADProvider({
-          clientId: clientId,
-          clientSecret: clientSecret,
-          tenantId: tenantId,
-          authorization: {
-            params: {
-              scope: `openid profile email offline_access api://${backendClientId}/user_impersonation`,
-              prompt: "login",
-            },
-          },
-        }),
-      ],
-      callbacks: {
-        async session({ session, token }) {
-          session.accessToken = token.accessToken as string;
-          console.log("session accessToken", session.accessToken);
+const authOptions: AuthOptions = {
+  secret: nextAuthSecret,
+  providers: [
+    AzureADProvider({
+      clientId: clientId,
+      clientSecret: clientSecret,
+      tenantId: tenantId,
+      authorization: {
+        params: {
+          scope: `openid profile email offline_access api://${backendClientId}/user_impersonation`,
+          prompt: "login",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
       if (token.error)
-            console.error(new Date(), "Error token in Session", token.error);
+        console.error(new Date(), "Error token in Session", token.error);
 
-          return session;
-        },
-        async jwt({ token, account }) {
-          if (account) {
-            console.log("IN ACCOUNT");
+      return session;
+    },
+    async jwt({ token, account }) {
+      if (account) {
         token.accessToken = account.access_token as string;
-            token.expiresAt = account.expires_at as number;
-            token.refreshToken = account.refresh_token;
-            return token;
-          } else if (Date.now() < token.expiresAt! * 1000) {
-            return token;
-          } else {
-            if (!token.refreshToken) throw new Error("Invalid refresh token");
-        console.log("IN REFRESH TOKEN", token.refreshToken);
-            try {
-              const response: Response = await fetch(
-                `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`,
-                {
-                  method: "POST",
-                  body: new URLSearchParams({
-                    client_id: clientId,
-                    client_secret: clientSecret,
-                    grant_type: "refresh_token",
-                    refresh_token: token.refreshToken!,
-                  }),
-                }
-              );
-
-              const tokensOrError = await response.json();
-
-              if (!response.ok) throw tokensOrError;
-
-              const newTokens = tokensOrError as {
-                access_token: string;
-                expires_in: number;
-                refresh_token?: string;
-              };
-              token.error = undefined;
-
-              return {
-                ...token,
-                accessToken: newTokens.access_token,
-                expiresAt: Math.floor(Date.now() / 1000 + newTokens.expires_in),
-                refreshToken: newTokens.refresh_token
-                  ? newTokens.refresh_token
-                  : token.refreshToken,
-              };
-            } catch (error) {
-              console.error(new Date(), "Error refreshing accessToken", error);
-              token.error = "RefreshTokenError";
-              return token;
+        token.expiresAt = account.expires_at as number;
+        token.refreshToken = account.refresh_token;
+        return token;
+      } else if (Date.now() < token.expiresAt! * 1000) {
+        return token;
+      } else {
+        if (!token.refreshToken) throw new Error("Invalid refresh token");
+        try {
+          const response: Response = await fetch(
+            `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`,
+            {
+              method: "POST",
+              body: new URLSearchParams({
+                client_id: clientId,
+                client_secret: clientSecret,
+                grant_type: "refresh_token",
+                refresh_token: token.refreshToken!,
+              }),
             }
-          }
-        },
-        async redirect({ url, baseUrl }) {
-      console.log("REDIRECT URL", url);
-      console.log("REDIRECT BASEURL", baseUrl);
+          );
+
+          const tokensOrError = await response.json();
+
+          if (!response.ok) throw tokensOrError;
+
+          const newTokens = tokensOrError as {
+            access_token: string;
+            expires_in: number;
+            refresh_token?: string;
+          };
+          token.error = undefined;
+
+          return {
+            ...token,
+            accessToken: newTokens.access_token,
+            expiresAt: Math.floor(Date.now() / 1000 + newTokens.expires_in),
+            refreshToken: newTokens.refresh_token
+              ? newTokens.refresh_token
+              : token.refreshToken,
+          };
+        } catch (error) {
+          console.error(new Date(), "Error refreshing accessToken", error);
+          token.error = "RefreshTokenError";
+          return token;
+        }
+      }
+    },
+    async redirect({ url, baseUrl }) {
       if (process.env.NODE_ENV === "development") {
         return url;
       }
