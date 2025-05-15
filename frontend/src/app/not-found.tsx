41ffeb8acc0cea18daf20ad0577d47a61106fae5
Albin Karlsson <albin.karlsson@bouvet.no>
2025-05-15 09:46:26 +0200

    feat: 163 not found page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
 import { Header } from "@/app/components/Header";
 import Footer from "@/app/components/Footer/Footer";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function NotFound() {
+export default async function NotFound() {
+  const headersList = await headers();
+  const cookie = headersList.get("cookie");
+  if (!cookie?.includes("next-auth.session-token")) {
+    redirect("/signin");
+  }
+
   return (
     <>
       <Header />
-      <main>
+      <main style={{ height: "calc(100dvh - 15rem)" }}>
         <section id="intro" className="survey-section">
-          <h3 className="mt-10 font-bold text-left">
+          <h1 className="mt-10 font-bold text-left">
             Ingen data for denne perioden
-          </h3>
+          </h1>
           <Link href="/" className="underline">
             Gå tilbake
           </Link>
