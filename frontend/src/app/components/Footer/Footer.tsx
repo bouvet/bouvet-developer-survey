63c5a5659946ed3ed4f1df797c1404cb1f5851ee
Albin Karlsson <albin.karlsson@bouvet.no>
2025-06-06 13:50:04 +0200

    mobile footer

--- frontend/src/app/components/Footer/Footer.tsx
+++ frontend/src/app/components/Footer/Footer.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 function Footer() {
   return (
-    <footer className="w-full h-40 bg-[#11133c] text-white mt-6 flex items-center justify-center">
+    <footer className="w-full h-20 md:h-40 bg-[#11133c] text-white mt-6 flex items-center justify-center">
       <p>Bouvet Developer Survey 2024</p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
