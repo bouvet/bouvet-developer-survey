bb5d6624a457ca2f9e064f168ada091a347a458f
Albin Karlsson <albin.karlsson@bouvet.no>
2025-06-22 17:56:02 +0200

    small footer stays longer

--- frontend/src/app/components/Footer/Footer.tsx
+++ frontend/src/app/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="w-full h-20 md:h-40 bg-[#11133c] text-white mt-6 flex items-center justify-center">
+    <footer className="w-full h-20 lg:h-40 bg-[#11133c] text-white mt-6 flex items-center justify-center">
       <p>Bouvet Developer Survey 2024</p>
     </footer>
   );
